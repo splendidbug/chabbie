e8daecc1bb52cecfa86adb8c85b220220c9019dd
shreyas <shreyasanother@gmail.com>
2020-01-21 02:31:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/chabbie/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.chabbie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("hmmm");
+        setContentView(R.layout.activity_main);
+
+    }
+}
